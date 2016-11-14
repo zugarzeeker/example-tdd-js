03f58b4f92116365c5fc9b0c7524947134609508
Supanut Apikulvanich <supanut.overload@gmail.com>
2016-11-14 09:21:15 +0700

    Prime number

---
+++ src/prime.js
@@ -0,0 +1,11 @@
+import _ from 'lodash'
+
+export const isPrime = (n) => {
+  return n !== 1 && !(
+    _.some(_.map(_.range(2, n - 1), i => n % i === 0))
+  ) || n == 2
+}
+
+export const generatePrime = (start, end) => (
+  _.range(start, end + 1).filter(isPrime)
+)
---
+++ src/prime.spec.js
@@ -0,0 +1,21 @@
+import { isPrime, generatePrime } from './prime'
+import assert from 'power-assert'
+
+describe('Prime', () => {
+  it('should be true if input is prime', () => {
+    assert(isPrime(2) === true)
+    assert(isPrime(3) === true)
+  })
+
+  it('should be false if input is not prime', () => {
+    assert(isPrime(1) === false)
+    assert(isPrime(4) === false)
+    assert(isPrime(6) === false)
+  })
+
+  it('should generate array of from start to end of input range', () => {
+    assert.deepEqual(generatePrime(2, 10), [2, 3, 5, 7])
+    assert.deepEqual(generatePrime(2, 2), [2])
+    assert.deepEqual(generatePrime(11, 17), [11, 13, 17])
+  })
+})
